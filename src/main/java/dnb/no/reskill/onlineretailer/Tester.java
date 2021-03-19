2b5250624d181e5a672a81a03cfb6b9b9c82feff
Knut Maurset <knut.espen.maurset@dnb.no>
2021-03-19 09:49:46 +0100

    push

--- src/main/java/dnb/no/reskill/onlineretailer/Tester.java
+++ src/main/java/dnb/no/reskill/onlineretailer/Tester.java
@@ -1,5 +1,6 @@
 package dnb.no.reskill.onlineretailer;
 
+import dnb.no.reskill.onlineretailer.bizlayer.ProductService;
 import dnb.no.reskill.onlineretailer.datalayer.ProductRepository;
 import dnb.no.reskill.onlineretailer.models.Product;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +17,9 @@ public class Tester {
     @Autowired
     private ProductRepository repository;
 
+    @Autowired
+    private ProductService productService;
+
         public void doDemo() {
 
             try {
@@ -28,7 +32,7 @@ public class Tester {
 
 
                 System.out.println("Adding real car, with id -1");
-                Product p10 = new Product(-1, "Realcar", 999, 11);
+                Product p10 = new Product(-1, "Real car", 12000, 2);
                 repository.addProduct(p10);
                 System.out.println("Id of " +p10.getName() + ": " +p10.getId());
 
@@ -52,11 +56,12 @@ public class Tester {
                 printAllProducts();
 
                 System.out.println("\ndoubling the stock of icecream and increase price by 10%");
-                Product p6 = repository.findProduct(8);
+                Product p6 = repository.findProduct(p5.getId());
                 p6.setInstock(150);
                 p6.setPrice(p6.adjustPriceByPercentage(10));
                 repository.updateProduct(p6);
-                System.out.println(repository.findProduct(8));
+                System.out.println(repository.findProduct(p6.getId()));
+                System.out.println("Displaying VAT: "+productService.getVat(p6.getId()));
 
 
             }
